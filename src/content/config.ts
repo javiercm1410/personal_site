038557505544046ecaea7dca0011d7593cf101eb
Javier Carrillo <jamaca1410@gmail.com>
2024-10-21 21:53:17 -0400

    feat: add blog post

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,13 +1,21 @@
 import { defineCollection, z} from "astro:content";
 
-const projectsCollection = defineCollection({
+const postsCollection = defineCollection({
+    type: 'content',
     schema: z.object({
         section: z.string(),
         date: z.string(),
-        title: z.string()
+        title: z.string(),
+        description: z.string(),
+        author: z.string(),
+        image: z.object({
+          url: z.string(),
+          alt: z.string()
+        }),
+        tags: z.array(z.string())
     }),
 });
 
 export const collection = {
-    projects: projectsCollection,
+    projects: postsCollection,
 };
